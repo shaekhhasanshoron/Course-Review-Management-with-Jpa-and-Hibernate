8b37ba0897f3a8f4942346bf84a443caf41ea9ea
shaekhhasanshoron <shaekhhasanshoron@gmail.com>
2018-05-09 16:19:58 +0600

    create course entity

---
+++ src/main/java/com/shoron/jpa/jpaandhibernate/model/Course.java
@@ -0,0 +1,37 @@
+package com.shoron.jpa.jpaandhibernate.model;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String name;
+
+	//Jpa should have aempty constructor
+	public Course(){		
+	}
+	public Course(String name) {
+		super();
+		this.name = name;
+	}
+	// only can get
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
