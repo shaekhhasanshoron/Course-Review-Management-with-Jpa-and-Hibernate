ba09404f32d26c4ba4843f29404c0a90d3983de0
shaekhhasanshoron <shaekhhasanshoron@gmail.com>
2018-05-09 16:13:43 +0600

    first commit

---
+++ src/main/java/com/shoron/jpa/jpaandhibernate/JpaandhibernateApplication.java
@@ -0,0 +1,12 @@
+package com.shoron.jpa.jpaandhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaandhibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaandhibernateApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shoron/jpa/jpaandhibernate/JpaandhibernateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shoron.jpa.jpaandhibernate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaandhibernateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
