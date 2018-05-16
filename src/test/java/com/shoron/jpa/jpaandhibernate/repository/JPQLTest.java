255792978bf094a43a7b58cf192a7d038d79cc26
shaekhhasanshoron <shaekhhasanshoron@gmail.com>
2018-05-16 11:07:27 +0600

    starting testing with jpql

---
+++ src/test/java/com/shoron/jpa/jpaandhibernate/repository/JPQLTest.java
@@ -0,0 +1,46 @@
+package com.shoron.jpa.jpaandhibernate.repository;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.shoron.jpa.jpaandhibernate.JpaandhibernateApplication;
+import com.shoron.jpa.jpaandhibernate.model.Course;
+import com.shoron.jpa.jpaandhibernate.model.Review;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=JpaandhibernateApplication.class)
+public class JPQLTest {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	CourseRepository repository;
+	
+	@Autowired
+	EntityManager em;
+
+	@Test
+	public void jpql_course_without_students(){
+		TypedQuery<Course> query = em.createQuery("Select c from Course c where c.students is empty", Course.class);
+		
+		List<Course> resultList = query.getResultList();
+		
+		logger.info("Course which do not have students -> {}", resultList);
+	}
+
+}
